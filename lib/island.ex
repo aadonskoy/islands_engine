9f3f3fcfdf97cfc89b99dbfa55dd3896438a5155
Oleksii Donskoi <oleksii@MacBook-Air-Oleksii.local>
2021-10-21 22:14:01 +0300

    Updated book. Coordinates and island

--- lib/island.ex
+++ lib/island.ex
@@ -1,28 +1,36 @@
 defmodule IslandsEngine.Island do
-  alias IslandsEngine.Coordinate
+  alias IslandsEngine.{Coordinate, Island}
 
-  def start_link() do
-    Agent.start_link(fn -> [] end)
+  @enforce_keys [:coordinates, :hit_coordinates]
+  defstruct [:coordinates, :hit_coordinates]
+
+  def new(type, %Coordinate{} = upper_left) do
+    with [_|_] = offsets <- offsets(type),
+      %MapSet{} = coordinates <- add_coordinates(offsets, upper_left)
+    do
+      {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new}}
+    else
+      error -> error
+    end
   end
 
-  def replace_coordinates(island, new_coordinates) when is_list new_coordinates do
-    Agent.update(island, fn _state -> new_coordinates end)
+  defp offsets(:sqhare), do: [{0, 0}, {0, 1}, {1, 0}, {1, 1}]
+  defp offsets(:atoll), do: [{0, 0}, {0, 1}, {1, 1}, {2, 0}, {2,1}]
+  defp offsets(:dot), do: [{0, 0}]
+  defp offsets(:l_shape), do: [{0, 0}, {1, 0}, {2, 0}, {2, 1}]
+  defp offsets(:s_shape), do: [{0, 1}, {0, 2}, {1, 0}, {1, 1}]
+  defp offsets(_), do: {:error, :invalid_island_type}
+
+  defp add_coordinates(offsets, upper_left) do
+    Enum.reduce_while(offsets, MapSet.new, fn offset, acc ->
+      add_coordinate(acc, upper_left, offset)
+    end)
   end
 
-  def forested?(island) do
-    island
-    |> Agent.get(fn state -> state end)
-    |> Enum.all?(fn coord -> Coordinate.hit?(coord) end)
-  end
-
-  def to_string(island) do
-    "[" <> coordinate_strings(island) <> "]"
-  end
-
-  def coordinate_strings(island) do
-    island
-    |> Agent.get(fn state -> state end)
-    |> Enum.map(fn coord -> Coordinate.to_string(coord) end)
-    |> Enum.join(", ")
+  defp add_coordinate(coordinates, %Coordinate{row: row, col: col}, {row_offset, col_offset}) do
+    case Coordinate.new(row + row_offset, col + col_offset) do
+      {:ok, coordinate} -> {:cont, MapSet.put(coordinates, coordinate)}
+      {:error, :invalid_coordinate} -> {:halt, {:error, :invalid_coordinate}}
+    end
   end
 end
